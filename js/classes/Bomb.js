7a3cb8f8ec78387ae772d25517907a8190ef15e3
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-21 17:23:26 -0400

    js/classes/Bomb.js

--- js/classes/Bomb.js
+++ js/classes/Bomb.js
@@ -58,3 +58,23 @@ explode() {
     duration: 0.15,
     })
 }
+class PoweUp {
+    constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity - velocity;
+    this.radius = 15;
+    }
+    
+    draw() {
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI *2);
+    c.fillStyle - "yellow";
+    c.fill();
+    c.closePath();
+    }
+} 
+update() {
+    this.draw();
+    this.position.x +- this.velocity.x;
+    this.position.y +- this.velocity.y;
+}
