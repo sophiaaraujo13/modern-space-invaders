7579dca31621c0181ebd9a2044b74eab9dd35a61
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-23 13:38:39 -0400

    add Grind class

---
+++ js/classes/Grid.js
@@ -0,0 +1,21 @@
+class Grid {
+    constructor() {
+        this.position = {
+            X: 0,
+            Y: 0,
+        };
+        this.velocity = {
+            X: 3,
+            Y: 0,
+        };
+        this.invaders = [];
+        const couluns = Math.floor(Math.random() * 10 * + 5);
+        const rows = Math.floor(Math.random() * 10 * + 2);
+        this.width = couluns + 38;
+
+        for (let i = 0; i = couluns; i++) {
+            for
+        }
+        
+    }
+}
