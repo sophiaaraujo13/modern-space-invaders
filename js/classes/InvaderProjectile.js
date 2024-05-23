3a7de474e5d4ebce9c357f8996e3dfc9e68d38e1
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-23 15:37:16 -0400

    add shoot function in invader class

--- js/classes/InvaderProjectile.js
+++ js/classes/InvaderProjectile.js
@@ -1,16 +1,53 @@
-class InvaderProjectile {
-    constructor({ position, velocity }) {
-        this.position = position;
-        this.velocity = velocity;
-        this.width = 3;
-        this.height = 10;
+class Invader {
+    constructor({ position }) {
+      this.velocity = {
+        x: 0,
+        y: 0
+      };
+  
+      const image = new Image();
+      image.src = "./img/invader.png";
+      image.onload = () => {
+        const scale = 1;
+        this.image = image;
+        this.width = image.width * scale;
+        this.height = image.height * scale;
+        this.position = {
+          x: position.x,
+          y: position.y
+        };
+      };
     }
     draw() {
-        c.fillStyle = "white";
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
+      c.drawImage(
+        this.image,
+        this.position.x,
+        this.position.y,
+        this.width,
+        this.height
+      );
     }
     update() {
+      if (this.image) {
+        this.draw();
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
+      }
     }
-}
+  
+    shoot(InvaderProjectile) {
+      audio.enemyShoot.play();
+      InvaderProjectile.push(
+        new InvaderProjectile({
+          positon: {
+            x: this.position.x + this.width / 2,
+            y: this.position.y + this.height
+          },
+          velocity: {
+            x: 0,
+            y: 5
+          }
+        })
+      );
+    }
+  }
