56396c163efb8ba22da12e09efbab91e95636d31
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-23 13:53:41 -0400

    add update function

--- js/classes/Grid.js
+++ js/classes/Grid.js
@@ -1,21 +1,38 @@
 class Grid {
     constructor() {
-        this.position = {
-            X: 0,
-            Y: 0,
-        };
-        this.velocity = {
-            X: 3,
-            Y: 0,
-        };
-        this.invaders = [];
-        const couluns = Math.floor(Math.random() * 10 * + 5);
-        const rows = Math.floor(Math.random() * 10 * + 2);
-        this.width = couluns + 38;
+this.position = {
+    x: 0,
+    y: 0,
+    };
 
-        for (let i = 0; i = couluns; i++) {
-            for
-        }
-        
+    this.velocity = {
+        x: 3,
+        y: 0,
+    };
+    this.invaders = [];
+
+    const columns =  Math.floor(Math.random() * 10 + 5);
+    const rows = Math.floor(Math.random() * 10 + 2);
+    this.width = columns * 30;
+
+    for (let i = 0; i < columns; i++) {
+        for (let j = 0; j < rows; j++) {
+            this.invaders.push(new Invader({
+                position: {
+                    x: i * 30,
+                    y: j * 30,
+                }
+            })); 
+      }
     }
+  }
 }
+update() {
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    this.velocity.y = 0;
+    if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
+      this.velocity.x = -this.velocity.x * 1.15;
+      this.velocity.y = 30;
+    }
+  }
