a21fc137ceb38027ed30dfb27a8f50eb9ba4c0b1
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-23 16:34:40 -0400

    add  draw function Player class

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -21,6 +21,28 @@ class Player {
         };
       };
       this.particles = [];
-      theis.frames = 0
+      theis.frames = 0;
+    }
+    draw() {
+      c.save();
+      c.globalAlpha = this.opacity;
+      c.translate(
+        player.position.x + player.width / 2,
+        player.position.y + player.height / 2
+      );
+      c.rotate(this.rotate);
+      c.translate(
+        -player.position.x + player.width / 2,
+        -player.position.y + player.height / 2
+      );
+      c.drawImage(
+        this.image,
+        player.position.x,
+        player.position.y,
+        this.width,
+        this.height
+      );
+      
+      c.restore();
     }
   }
