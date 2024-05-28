fad70a01beff7c9421999c2eed122e7564cbca16
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-28 16:39:11 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const scoreEL = document.querySelector("#scoreEL");
+const canvas = document.querySelector("canvas");
+const c = canvasEl.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
