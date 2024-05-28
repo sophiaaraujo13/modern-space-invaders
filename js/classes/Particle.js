f084901172b56f4e21ebeb3583195e76b35136b2
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-28 16:00:10 -0400

    Particle.js

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -6,6 +6,22 @@ class Particle {
         this.color = color;
         this.opacity = 1;
         this.fades = fades;
-   
- }
+      }
+      draw() {
+        c.save();
+        c.globalAlpha = this.opacity;
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.fillStyle = this.color;
+        c.fill();
+        c.closePath();
+        c.restore();
+    }
+    update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.fades)  this.opacity += 0,01{
+   }
+  }
 }
