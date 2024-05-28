fd6257727d3c5cef9c71843bce9824359ac6386c
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-28 16:28:37 -0400

    update

--- js/classes/Particle.js
+++ js/classes/Particle.js
@@ -21,7 +21,7 @@ class Particle {
     this.draw();
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
-    if (this.fades)  this.opacity += 0,01{
+    if (this.fades)  this.opacity += 0,01 {
    }
   }
 }
--- js/classes/Projectile.js
+++ js/classes/Projectile.js
@@ -5,4 +5,21 @@ class Projectile {
         this.radius = 10;
     
 }
-}
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        c.closePath();
+        c.fillStyle = "white";
+        c.fill();
+        c.closePath();
+    }
+
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    
+    }
+    
+}   
