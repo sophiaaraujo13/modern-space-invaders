350a421aea94cfb9f2400a6ea8ce312b4ddbcb5f
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-05-28 15:38:17 -0400

    Particle.js

---
+++ js/classes/Particle.js
@@ -0,0 +1,11 @@
+class Particle {
+    constructor({ position, velocity, radius, color, fades }) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = radius;
+        this.color = color;
+        this.opacity = 1;
+        this.fades = fades;
+   
+ }
+}
