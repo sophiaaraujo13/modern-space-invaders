527a68adf51437940a6d7fab23c273f6ece31981
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-06-04 16:32:52 -0400

    util.js

--- util.js
+++ util.js
@@ -1,3 +1,25 @@
 function randomBetween(min, max) {
     return Math.random() * (max - min) + min;
 }
+
+function createScoreLabel({score = 100, object}) {
+    const scoreLabel = document.createElement("Label");
+
+    scoreLabel.innerHTML = score;
+    scoreLabel.style.position = "absolute";
+    scoreLabel.style.color = "white";
+    scoreLabel.style.top = object.position.y + "px";
+    scoreLabel.style.left = object.position.x + "px";
+    scoreLabel.style.userSelect = "none";
+
+    document.querySelectorAll("#parentDiv").appendChild(scoreLabel);
+
+    gsap.to(scoreLabel, {
+        opacity: 0,
+        y: -30,
+        duration: 0.75,
+        onComplete: () => {
+
+        }
+    });
+}
