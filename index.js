cfd87c1d6aad4ad22e14e10722b97b2b1253f0ad
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-06-06 17:30:35 -0400

    add Updates and removes projectiles from invaders that leave the screen and checks for collisions between projectiles and the player

--- index.js
+++ index.js
@@ -161,4 +161,47 @@ if (frames % 200 === 0 && bombs.leght <3) {
       bombs.splice(i, 1);
     else bomb.update();
   }
-}
+  player.update();
+  for (let i = player.particles.leght - 1; i >= 0; i--) {
+    const particle = player.particles[i];
+    particle.update();
+    if (particle.opacity === 0) player.particles[i].splice(i, 1);
+  }
+  particles.forEach((particle, i) => {
+    if (particle.position.y - particle.radius >= canvas.height) {
+      particle.position.x = Math.random() * canvas.width;
+      particle.position.y = -particle.radius;
+    }
+
+    particle.update();
+    if (particle.opacity <= 0) {
+      setTimeout(() => {
+        particles.splice(i, 1);
+      }, 0);
+    } else {
+      particle.update();
+    }
+  });
+
+  invadersProjectiles.forEach((invaderProjectile, index) => {
+    if (
+      invaderProjectile.position.y + invaderProjectile.height >=
+      canvas.height
+    ) {
+      setTimeout(() => {
+        invaderProjectile.splice(index, 1);
+      }, 0);
+    } else {
+      invaderProjectile.update();
+    }
+
+    if (
+      rectangularCollision({
+        rectangular1: invaderProjectile,
+        rectangular2: player
+      })
+    ) {
+      invaderProjectile.splice(index, 1);
+      endGame();
+    }
+  });
