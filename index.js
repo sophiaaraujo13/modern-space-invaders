d836c02466fb124cc551b65e53f600968553d7a7
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-06-06 14:02:06 -0400

    add game control variables

--- index.js
+++ index.js
@@ -4,3 +4,39 @@ const c = canvasEl.getContext("2d");
 
 canvas.width = 1024;
 canvas.height = 576;
+
+let projectiles = [];
+let grids = [];
+let invadersProjectiles = [];
+let particles = [];
+let bombs = [];
+let powerUps = [];
+
+let player = new Player();
+
+let keys = {
+  ArrowLeft: {
+    pressed: false
+  },
+  ArrowRight: {
+    pressed: false
+  },
+  Space: {
+    pressed: false
+  }
+};
+
+let frames = 0;
+let randomInterval = Math.floor(Math.random() * 500 + 500);
+
+let game = {
+    over: false,
+    active: true
+};
+
+let score = 0;
+let spawnBuffer = 500;
+let fps = 60;
+let fpsInterval = 1000 / fps;
+
+let msPrev = window.performance.now();
