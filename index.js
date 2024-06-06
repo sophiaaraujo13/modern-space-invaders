75575e31dbce02f91ce8f8e11d3923b19203ef6b
sophiaaraujo13 <sophiaaraujo640@gmail.com>
2024-06-06 14:33:17 -0400

    add init function index.js file

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ let frames = 0;
 let randomInterval = Math.floor(Math.random() * 500 + 500);
 
 let game = {
-    over: false,
-    active: true
+  over: false,
+  active: true
 };
 
 let score = 0;
@@ -40,3 +40,53 @@ let fps = 60;
 let fpsInterval = 1000 / fps;
 
 let msPrev = window.performance.now();
+
+function init() {
+  player = new Player();
+  projectiles = [];
+  grids = [];
+  invadersProjectiles = [];
+  particles = [];
+  bombs = [];
+  powerUps = [];
+  frames = 0;
+
+  keys = {
+    ArrowLeft: {
+      pressed: false
+    },
+    ArrowRight: {
+      pressed: false
+    },
+    Space: {
+      pressed: false
+    }
+  };
+
+  frames = 0;
+  randomInterval = Math.floor(Math.random() * 500 + 500);
+
+  game = {
+    over: false,
+    active: true
+  };
+
+  score = 0;
+
+  for (let i = 0; i < 100; i++) {
+    particles.push(
+      new Particle({
+        position: {
+          x: Math.random() * canvas.width,
+          y: Math.random() * canvas.height
+        },
+        velocity: {
+          x: 0,
+          y: 0.3
+        },
+        radius: Math.random() * 2,
+        color: "white"
+      })
+    );
+  }
+}
